7e08aff6abb77d5ea316a9a7618d191191dfe222
Truong Van Tri <38130254+truonglammoc@users.noreply.github.com>
2019-06-06 07:39:41 +0700

    Update uiza.php

--- uiza.php
+++ uiza.php
@@ -7,7 +7,7 @@ require_once "common.php";
  * Take this as a base plugin and modify as per your need.
  *
  * @package Uiza Plugin
- * @author Tritv
+ * @author Uiza
  * @license GPL-2.0+
  * @link https://uiza.io/
  * @copyright 2019 Uiza, LLC. All rights reserved.
@@ -20,7 +20,7 @@ require_once "common.php";
  *            Author: Tritv
  *            Author URI: https://uiza.io/
  *            Text Domain: uiza
- *            Contributors: Tritv
+ *            Contributors: Uiza
  *            License: GPL-2.0+
  *            License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
@@ -31,12 +31,3 @@ wp_enqueue_script('aws-sdk-2.283.1.min.js', 'https://sdk.amazonaws.com/js/aws-sd
 add_action("admin_menu", "uiza_add_menu");
 
 add_action("admin_init", "uiza_settings");
-
-// add_action('admin_menu', 'add_custom_link_into_uiza_menu');
-
-// /**
-//  * Append saved textfield value to each post
-//  *
-//  * @since 1.0
-//  */
-// add_filter('the_content', 'com_content');
