aa269c1a378dd7165beda6d132f61e2182f5616e
Tri Truong <vtritruong@gmail.com>
2019-06-10 21:01:04 +0700

    Fixed css upload file

--- create_task.php
+++ create_task.php
@@ -8,7 +8,7 @@
     <div class="col-md-6">
         <div class="form-group files">
             <label>Upload Your Video </label>
-            <input type="file" class="form-control" onchange="changeFile(event)">
+            <input type="file" class="form-control" id="select_file_upload" onchange="changeFile(event)">
             <input type="hidden" id="api-key-h" value="<?=get_option('uiza-api-key')?>">
         </div>
         <button type="submit" onclick="submitUpload()" class="btn btn-primary">Process</button>
